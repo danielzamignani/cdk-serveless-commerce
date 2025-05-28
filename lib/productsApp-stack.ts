a432135e733d4788860c31f2309b6ce7f8abc1c3
Daniel Zamignani <danielzamignani@gmail.com>
2025-05-28 18:06:53 -0300

    feat: add lambda insights

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -46,7 +46,8 @@ export class ProductsAppStack extends cdk.Stack {
                 PRODUCTS_DDB: this.productsDdb.tableName
             },
             layers: [productsLayer],
-            tracing: lambda.Tracing.ACTIVE
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
         });
 
         this.productsDdb.grantReadData(this.procuctsFetchHandler);
@@ -69,7 +70,8 @@ export class ProductsAppStack extends cdk.Stack {
                 PRODUCTS_DDB: this.productsDdb.tableName
             },
             layers: [productsLayer],
-            tracing: lambda.Tracing.ACTIVE  
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
         });
 
         this.productsDdb.grantWriteData(this.procuctsAdminHandler);
